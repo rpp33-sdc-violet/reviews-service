ea8324ca810f03bc6bffdabf14dceaa225de9cbf
Joann Whang <joann.whang@gmail.com>
2022-03-21 20:29:53 -0500

    move server files to root directory

--- server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/test', (req, res) => {
-  res.send('testing...testing...1, 2, 3');
-});
-
-module.exports = app;
--- server/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-const port = 8080;
-
-app.listen(port, () => {
-  console.log(`Reviews Server: Listening on port ${port}`);
-});
