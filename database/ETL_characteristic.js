2e05f8610a490d0e37cd3bf75f4c5d40fd0ff0b2
Joann Whang <joann.whang@gmail.com>
2022-03-23 01:46:32 -0500

    feat: ETL process complete for characteristics.csv into characteristic table

---
+++ database/ETL_characteristic.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { Pool } = require('pg');
+const fastcsv = require('fast-csv');
+require('dotenv').config();
+
+const csvOptions = { headers: true };
+
+// create a new connection to the database
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err;
+
+  const readable = fs.createReadStream('../../RPP33_General/SDC_data/characteristics.csv')
+    .pipe(fastcsv.parse(csvOptions))
+    .on('error', (error) => {
+      console.log(error);
+      process.exit();
+    })
+    .on('data', (row) => {
+      const formatData = [Number(row.id), Number(row.product_id), row.name];
+      const query = 'INSERT INTO characteristic (characteristic_id, product_id, category) VALUES ($1, $2, $3)';
+      readable.pause();
+      client.query(query, formatData, (error, res) => {
+        if (error) {
+          console.log('ERROR IN INSERT', error);
+        } else {
+          readable.resume();
+          // BELOW CODE USED ONLY FOR TESTING SMALL CSV FILES:
+          // console.log('SUCCESS IN INSERT', res);
+        }
+      });
+    })
+    .on('end', (rowCount) => {
+      done();
+      console.log('finished ETL process - rowCount:', rowCount);
+    });
+});
