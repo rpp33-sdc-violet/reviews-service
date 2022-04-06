cfee63fed8ce6e4b113b90558f1680a94d4848bf
Joann Whang <joann.whang@gmail.com>
2022-04-05 23:24:47 -0400

    feat: GET /reviews complete

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,18 @@ const models = require('../models/index');
 module.exports = {
   reviews: {
     get: (req, res) => {
-      models.reviews.get()
-        .then((data) => {
-          console.log('CONTROLLER GET DATA HERE', data);
+      const page = !req.query.page ? 1 : Number(req.query.page);
+      const count = !req.query.count ? 5 : Number(req.query.count);
+      const { sort } = req.query;
+      const productId = req.query.product_id;
+
+      models.reviews.get(page, count, sort, productId, (err, data) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
           res.send(data);
-        })
-        .catch((error) => {
-          res.status(500).send('error in GET /reviews');
-        });
+        }
+      });
     },
     post: (req, res) => {
       if (models.reviews.post() === 'reviews data posted') {
--- models/index.js
+++ models/index.js
@@ -3,42 +3,19 @@ const pool = require('../database/index');
 
 module.exports = {
   reviews: {
-    get: () => {
-      console.log('in models reviews GET');
-      // hardcoded parameters - TODO: get FE params
-      const page = 1;
-
-      let offsetPage;
-      // page - if 0, change to 1
-      if (page === 0) {
-        offsetPage = 1;
-      } else {
-        offsetPage = page;
-      }
-
-      const count = 2;
+    get: (page, count, sort, productId, callback) => {
+      const offsetPage = page === 0 ? 1 : page;
       const limit = count;
       const offset = (offsetPage - 1) * count;
-      // conditional sorting
-      // helpfulness: helpfulness DESC
-      // newest: date DESC
-      // relevant: helpfulness DESC, date DESC
-      const sort = 'helpfulness DESC, date DESC';
-      const productId = 2;
 
-      const queryReviewWithoutPhotosTEST = `
-        (SELECT json_agg(row_to_json(reviews)) 
-        FROM (
-          SELECT review_id, rating, summary, recommend, body, date, reviewer_name, helpfulness  
-          FROM review 
-          WHERE product_id=${productId}
-        ) AS reviews)`;
-
-      const queryPhotosTEST = `
-        SELECT json_agg(row_to_json(photos))
-        FROM (
-          SELECT photo_id, url FROM photo WHERE review_id=${5}
-        ) as photos`;
+      let sortText;
+      if (sort === 'newest') {
+        sortText = 'date DESC';
+      } else if (sort === 'helpful') {
+        sortText = 'helpfulness DESC';
+      } else if (sort === 'relevant') {
+        sortText = 'helpfulness DESC, date DESC';
+      }
 
       const queryText = `
         SELECT json_build_object(
@@ -55,19 +32,34 @@ module.exports = {
             ) AS photos  
           FROM review 
           WHERE product_id=${productId} AND reported=false
-          ORDER BY ${sort}
+          ORDER BY ${sortText}
           LIMIT ${limit}
           OFFSET ${offset}
         ) AS allReviews))`;
 
-      return pool.query(queryText)
-        .then((res) => {
-          console.log('GET /reviews:', res.rows[0].json_build_object);
-          return res.rows[0].json_build_object;
-        })
-        .catch((error) => {
-          console.log('error in models reviews GET', error);
-        });
+      pool.query(queryText, (err, res) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, res.rows[0].json_build_object);
+        }
+      });
+
+      /* QUERY INVESTIGATION
+      const queryReviewWithoutPhotosTEST = `
+        (SELECT json_agg(row_to_json(reviews))
+        FROM (
+          SELECT review_id, rating, summary, recommend, body, date, reviewer_name, helpfulness
+          FROM review
+          WHERE product_id=${productId}
+        ) AS reviews)`;
+
+      const queryPhotosTEST = `
+        SELECT json_agg(row_to_json(photos))
+        FROM (
+          SELECT photo_id, url FROM photo WHERE review_id=${5}
+        ) as photos`;
+      */
     },
     post: () => {
       console.log('in models reviews POST');
