b4ab5fd172c29528214a8353609e180305cd03ff
Joann Whang <joann.whang@gmail.com>
2022-04-05 22:32:03 -0400

    WIP: GET /reviews

--- models/index.js
+++ models/index.js
@@ -3,16 +3,30 @@ const pool = require('../database/index');
 
 module.exports = {
   reviews: {
-    get: async () => {
+    get: () => {
       console.log('in models reviews GET');
       // hardcoded parameters - TODO: get FE params
       const page = 1;
-      const count = 10;
-      const sortNew = 'newest';
-      const sortHelp = 'helpful';
-      const sortRel = 'relevant';
+
+      let offsetPage;
+      // page - if 0, change to 1
+      if (page === 0) {
+        offsetPage = 1;
+      } else {
+        offsetPage = page;
+      }
+
+      const count = 2;
+      const limit = count;
+      const offset = (offsetPage - 1) * count;
+      // conditional sorting
+      // helpfulness: helpfulness DESC
+      // newest: date DESC
+      // relevant: helpfulness DESC, date DESC
+      const sort = 'helpfulness DESC, date DESC';
       const productId = 2;
-      const queryText1 = `
+
+      const queryReviewWithoutPhotosTEST = `
         (SELECT json_agg(row_to_json(reviews)) 
         FROM (
           SELECT review_id, rating, summary, recommend, body, date, reviewer_name, helpfulness  
@@ -20,41 +34,36 @@ module.exports = {
           WHERE product_id=${productId}
         ) AS reviews)`;
 
-      const queryText2 = `
+      const queryPhotosTEST = `
         SELECT json_agg(row_to_json(photos))
         FROM (
           SELECT photo_id, url FROM photo WHERE review_id=${5}
         ) as photos`;
 
-      const queryText3 = `
-        SELECT json_agg(row_to_json(reviews)) 
+      const queryText = `
+        SELECT json_build_object(
+        'product', ${productId},
+        'page', ${page},
+        'count', ${count},
+        'results', (SELECT json_agg(row_to_json(allReviews)) 
         FROM (
-          SELECT review_id, rating, summary, recommend, body, date, reviewer_name, helpfulness,
-          (SELECT json_agg(photos)
-          FROM (
-            SELECT photo_id, url FROM photo WHERE review_id=review.review_id
-          ) as photos)  
-          FROM review 
-          WHERE product_id=${productId}
-        ) AS reviews`;
-
-      const queryText4 = `
-        SELECT json_agg(row_to_json(allReviews)) 
-        FROM (
-          SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness,
+          SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, reported,
             (SELECT array_to_json(array_agg(allPhotos))
               FROM (
                 SELECT photo_id AS id, url FROM photo WHERE review_id=review.review_id
               ) allPhotos
             ) AS photos  
           FROM review 
-          WHERE product_id=${productId}
-        ) AS allReviews`;
+          WHERE product_id=${productId} AND reported=false
+          ORDER BY ${sort}
+          LIMIT ${limit}
+          OFFSET ${offset}
+        ) AS allReviews))`;
 
-      return pool.query(queryText4)
+      return pool.query(queryText)
         .then((res) => {
-          console.log('HERE:', res.rows[0].json_agg);
-          return 'test';
+          console.log('GET /reviews:', res.rows[0].json_build_object);
+          return res.rows[0].json_build_object;
         })
         .catch((error) => {
           console.log('error in models reviews GET', error);
