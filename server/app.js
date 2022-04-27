28f80c4e004578c449950f19d742b1cdeb4237b3
EC2 Default User <ec2-user@ip-172-31-19-54.ec2.internal>
2022-04-27 02:16:19 +0000

    undo newrelic install

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('../database/index');
+const path = require('path');
 
 app.use(express.json());
 
@@ -13,6 +14,10 @@ app.use('/reviews_test', router);
 
 app.get('/test', (req, res) => {
   res.send('testing...testing...1, 2, 3');
+});
+
+app.get('/loaderio-bb7ccf49217978042ae9eae0a98aa8af', (req, res) => {
+  res.send('loaderio-bb7ccf49217978042ae9eae0a98aa8af');
 });
 
 module.exports = app;
